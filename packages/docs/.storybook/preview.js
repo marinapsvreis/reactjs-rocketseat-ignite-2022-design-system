c07d06fe2ab68b9eafab9e8da9c50e32ef0cb46c
Marina Portugal <marinapsvreis@gmail.com>
2023-06-06 20:57:22 -0300

    :lipstick: Dark theme to Storybook

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,6 +1,19 @@
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'light',
+          value: '#F8F8F8',
+        },
+        {
+          name: 'dark',
+          value: '#333333',
+        },
+      ],
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
