c24c8088c3a8ff018d0fbc120cdd4688c88825bd
Marina Portugal <marinapsvreis@gmail.com>
2023-06-08 19:45:03 -0300

    :wrench: Update repo name to my repo

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/reactjs-rocketseat-ignite-2022-design-system/'
     }
 
     return config
