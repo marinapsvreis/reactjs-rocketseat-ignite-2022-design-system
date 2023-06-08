2a3bd83328063de98887dea9f6e4804812bfc30b
Marina Portugal <marinapsvreis@gmail.com>
2023-06-08 19:38:58 -0300

    :wrench: Downgrade Storybook version

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,32 +1,14 @@
-import { themes } from '@storybook/theming';
+import { themes } from '@storybook/theming'
 
-/** @type { import('@storybook/react').Preview } */
-const preview = {
-  parameters: {
-    backgrounds: {
-      default: 'dark',
-      values: [
-        {
-          name: 'light',
-          value: '#F8F8F8',
-        },
-        {
-          name: 'dark',
-          value: '#333333',
-        },
-      ],
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-    docs: {
-      theme: themes.dark
-    }
   },
-};
-
-export default preview;
+  docs: {
+    theme: themes.dark,
+  }
+}
