d840fba16361354484ae12e39ebbfb97688e1654
Marina Portugal <marinapsvreis@gmail.com>
2023-06-08 14:47:06 -0300

    :sparkles: Creating Text and Box components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { Text, TextProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde minima, aperiam dignissimos perferendis dolores iure vel beatae ullam. Nam vel atque nulla ipsa, debitis eos repudiandae mollitia inventore autem? Enim.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
---
+++ packages/react/src/components/Box.tsx
@@ -0,0 +1,13 @@
+import { ComponentProps, ElementType } from 'react'
+import { styled } from '../styles'
+
+export const Box = styled('div', {
+  padding: '$4',
+  backgroundColor: '$gray800',
+  borderRadius: '$md',
+  border: '1px solid $gray600',
+})
+
+export interface BoxProps extends ComponentProps<typeof Box> {
+  as?: ElementType
+}
